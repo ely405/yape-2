2468f220f372799270860a26a7f9c0df73320d82
ely211 <gonzalesm405@gmail.com>
2017-06-21 17:59:26 -0500

    segundo hito

--- public/js/index.js
+++ public/js/index.js
@@ -30,4 +30,8 @@ const documentLoad = ()=>{
 $(document).ready(function(){
   documentLoad();
   $('.carousel').carousel();
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 5 // Creates a dropdown of 15 years to control year
+  });
 });
